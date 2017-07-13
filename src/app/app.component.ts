2d2a730d9548d5ee1274f461db457f5f6a35b51e
Moin Shaikh <shaikhmoin250@gmail.com>
2017-07-13 18:43:01 +0530

    My all codes are there

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MyDataService} from './my-data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private newService:MyDataService){}
+
+  ngOnInit(){
+   //this.newService.obj.name="JOHN";
+   //onsole.log(this.newService.obj.name)
+    this.newService.fetchData();  
 }
+
+
+  title = 'moin!';
+  day= new Date();
+  obj={
+    id :1,
+    name: "moin"
+  }
+
+  arr=[4,5,6,3,4,2,1,4,6,8];
+  isTrue= true;
+  myName="moin";
+
+  items= ["Angular 4", "React", "Underscore"];
+  newItem="";
+  pushItem= function() {
+    if(this.newItem != "")
+    {
+      this.items.push(this.newItem);
+      this.newItem="";
+    }
+  }
+  removeItem= function(index)
+  {
+    this.items.splice(index, 1);
+  }
+}
+
