ae2fa074d9ee807cd618127f96ab1c5d8150bb18
Moin Shaikh <shaikhmoin250@gmail.com>
2017-07-13 18:41:22 +0530

    My import files are there

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from'@angular/router';
 
 import { AppComponent } from './app.component';
+import {MyDataService} from './my-data.service';
+import { MyLoginComponent } from './my-login/my-login.component';
+import { UsersComponent } from './users/users.component';
+import { SortPipe } from './app.sort';
 
 @NgModule({
   declarations: [
-    AppComponent
+     SortPipe,
+    MyLoginComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+     RouterModule.forRoot([
+     {
+       path:'users',
+      component: UsersComponent
+     },
+     {
+       path:'my-login',
+       component: MyLoginComponent
+     }
+    ])
   ],
-  providers: [],
+  providers: [MyDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
